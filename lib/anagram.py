4ff969d523d86c5e12a34d7da71483731ba3557c
Sifa Gakeni Muriithi <gakenisifa@gmail.com>
2024-12-02 17:31:35 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+anagram = ['enlists', 'google', 'inlets', 'banana']
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagram):
+        sort_words = sorted(self.word)
+        matches = [anagram for anagram in anagram if sort_words == sorted(anagram)]
+        return matches
+    
+checker = Anagram("listen")
+anagram = checker.match(anagram)
+print(anagram)
